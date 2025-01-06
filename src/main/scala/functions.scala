05c1f789f0eda4f0a9780e1741ac1a2056eaa9b7
Superpihl <22777994+Superpihl@users.noreply.github.com>
2025-01-06 15:19:43 +0100

    Update functions.scala

--- src/main/scala/functions.scala
+++ src/main/scala/functions.scala
@@ -4,11 +4,10 @@ import chisel3.util._
 class functions extends Module {
   val io = IO(new Bundle {
     val instr = Input(UInt(32.W))
-    val decOut = decOut(new Bundle {
-      val aluOp = Output(UInt(32.W))
-      val imm = Output(SInt(32.W))
-      val instrType = Output(UInt(4.W))
-      
+    val aluOp = Output(UInt(32.W))
+    val imm = Output(SInt(32.W))
+    val instrType = Output(UInt(4.W))
+
     })
   })
 }
